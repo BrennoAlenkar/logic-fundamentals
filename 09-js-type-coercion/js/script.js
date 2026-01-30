1895e58cac9004890b70ce980706c5c313da0770
Brenno Alencar <127119853+BrennoAlenkar@users.noreply.github.com>
2026-01-30 10:43:02 -0300

    adiciona explicação e exemplos de coerção de tipos

---
+++ 09-js-type-coercion/js/script.js
@@ -0,0 +1,22 @@
+   // Variáveis
+    const minhaIdade = 28; // number
+    const anoAtual = 2026; // number
+    const nome = "Brenno"; // string
+
+    console.log(nome + " sua idade é: " + minhaIdade); // Concatenação
+    console.log(minhaIdade + " é minha idade!"); // Concatenação
+
+    console.log(25 + 25); // soma = 50
+    console.log(25 + "25"); // concatenação = 2525
+    console.log("25" + "25"); // concatenação = 2525
+    console.log("25 + 25"); // string = "25 + 25"
+
+    console.log("25" + 10); // concatenação = 2510
+    console.log("25" - 10); // conversão automática = 15
+    console.log("10" * 10); // conversão automática = 100
+    console.log("10" / 10); // conversão automática = 1
+
+    console.log("string" + 10 + 10); // concatenação = string1010
+    console.log("string" + 10 - 10); // NaN
+    console.log("string" + (10 * 10)); // multiplicação = string100
+    console.log("string" + (10 / 10)); // divisão = string1
